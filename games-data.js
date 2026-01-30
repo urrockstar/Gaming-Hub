05a824c117dcb85a63df4c0cc6a9636e0356e93b
urrockstar <urrockstar04@gmail.com>
2026-01-30 18:45:59 +0530

    Add files via upload

---
+++ games-data.js
@@ -0,0 +1,45 @@
+const gamesData = {
+  puzzle: [
+    { title: "Sudoku", description: "The classic number puzzle loved worldwide.", image: "../images/sudoku.jpg", link: "../games/sudoku.html" },
+    { title: "Tetris", description: "Fit the blocks and clear rows in this timeless puzzle.", image: "../images/tetris.jpg", link: "../games/tetris.html" },
+    { title: "Candy Crush Saga", description: "Match candies and complete challenging levels.", image: "../images/candycrush.jpg", link: "../games/candy-crush.html" },
+    { title: "2048", description: "Merge numbers strategically to reach 2048.", image: "../images/2048.jpg", link: "../games/2048.html" },
+    { title: "Monument Valley", description: "A stunning puzzle game with impossible architecture.", image: "../images/monument-valley.jpg", link: "../games/monument-valley.html" },
+    { title: "Portal", description: "Solve 3D puzzles using portals in this mind-bending adventure.", image: "../images/portal.jpg", link: "../games/portal.html" }
+  ],
+
+  action: [
+    { title: "Call of Duty", description: "Fast-paced first-person shooter.", image: "../images/cod.jpg", link: "../games/cod.html" },
+    { title: "Fortnite", description: "Battle Royale with building mechanics.", image: "../images/fortnite.jpg", link: "../games/fortnite.html" },
+    { title: "PUBG", description: "Survive and be the last one standing.", image: "../images/pubg.jpg", link: "../games/pubg.html" },
+    { title: "DOOM", description: "Classic demon-slaying FPS action.", image: "../images/doom.jpg", link: "../games/doom.html" }
+  ],
+
+  multiplayer: [
+    { title: "Among Us", description: "Find the imposter in this social deduction game.", image: "../images/amongus.jpg", link: "../games/amongus.html" },
+    { title: "League of Legends", description: "Team-based MOBA strategy game.", image: "../images/lol.jpg", link: "../games/lol.html" },
+    { title: "Minecraft", description: "Build and explore worlds with friends.", image: "../images/minecraft.jpg", link: "../games/minecraft.html" },
+    { title: "CS:GO", description: "Competitive tactical shooter.", image: "../images/csgo.jpg", link: "../games/csgo.html" }
+  ],
+
+  adventure: [
+    { title: "The Legend of Zelda", description: "Explore dungeons and defeat bosses.", image: "../images/zelda.jpg", link: "../games/zelda.html" },
+    { title: "Assassin's Creed", description: "Historical action-adventure stealth series.", image: "../images/ac.jpg", link: "../games/ac.html" },
+    { title: "Uncharted", description: "Treasure hunting adventure.", image: "../images/uncharted.jpg", link: "../games/uncharted.html" },
+    { title: "Tomb Raider", description: "Explore ruins and solve mysteries with Lara Croft.", image: "../images/tombraider.jpg", link: "../games/tombraider.html" }
+  ],
+
+  sports: [
+    { title: "FIFA", description: "Play football with realistic graphics and physics.", image: "../images/fifa.jpg", link: "../games/fifa.html" },
+    { title: "NBA 2K", description: "Basketball simulation at its best.", image: "../images/nba2k.jpg", link: "../games/nba2k.html" },
+    { title: "Rocket League", description: "Cars + soccer = chaos.", image: "../images/rocketleague.jpg", link: "../games/rocketleague.html" },
+    { title: "Tony Hawk's Pro Skater", description: "Skateboarding legend.", image: "../images/thps.jpg", link: "../games/thps.html" }
+  ],
+
+  strategy: [
+    { title: "Clash of Clans", description: "Build, raid, and defend your village.", image: "../images/coc.jpg", link: "../games/coc.html" },
+    { title: "Age of Empires", description: "Classic RTS empire building.", image: "../images/aoe.jpg", link: "../games/aoe.html" },
+    { title: "Civilization VI", description: "Build a civilization that stands the test of time.", image: "../images/civ6.jpg", link: "../games/civ6.html" },
+    { title: "StarCraft II", description: "Legendary sci-fi RTS battles.", image: "../images/sc2.jpg", link: "../games/sc2.html" }
+  ]
+};
